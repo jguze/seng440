7749351606e52c1973c5849d871f25bfc1a1cab1
Justin Guze <jaguze@gmail.com>
2014-07-14 12:14:18 -0700

    Attempted to make nicer fixed point arithmetic work

--- cordic.c
+++ cordic.c
@@ -19,10 +19,20 @@ double elem_angle[] = {
 typedef struct vector3 {
 	int x;
 	int y;
-	double z;
+	int z;
 } vector3;
 
 typedef enum {ROTATION, VECTOR} cordic_mode_t;
+
+double fixed_to_float(int fixed)
+{
+	return ( (double) (fixed & SHIFT_MASK) / (1 << SHIFT) );
+}
+
+int float_to_fixed(double flt)
+{
+	return (int)(flt * 65536);
+}
 
 /*
  * This was supposed to return the appropriate true or false
@@ -60,13 +70,14 @@ int rot_decision(cordic_mode_t mode, void * val)
  * y: The y coordinate in the vector.
  * z: The angle accumulator. Reaches 0 after n iterations
  */ 
-vector3 cordic_rotation(int x, int y, double z)
+vector3 cordic_rotation(int x, int y, int z)
 {
 	int i;
 	int x_temp;
 	
 	x = x << SHIFT;
 	y = y << SHIFT;
+	z = z << SHIFT;
 	
 	for (i = 0; i < ELEM_SIZE; i++)
 	{
@@ -75,11 +86,11 @@ vector3 cordic_rotation(int x, int y, double z)
 		{
 			x = x + (y >> i);
 			y = y - (x_temp >> i);
-			z = z + elem_angle[i]; 
+			z = z + float_to_fixed(elem_angle[i]); 
 		} else {
 			x = x - (y >> i);
 			y = y + (x_temp >> i);
-			z = z - elem_angle[i];
+			z = z - float_to_fixed(elem_angle[i]);
 		}
 	}
 
@@ -97,14 +108,15 @@ vector3 cordic_rotation(int x, int y, double z)
  * y: The y coordinate of the vector. Reaches 0 after n iterations.
  * z: The angle accumulator. Reaches z[0] + arctan(y[0]/x[0]) after n iterations.
  */
-vector3 cordic_vector(int x, int y, double z)
+vector3 cordic_vector(int x, int y, int z)
 {
 	int i;
 	int x_temp;
 	
 	x = x << SHIFT;
 	y = y << SHIFT;
-
+	z = z << SHIFT;
+	
 	for (i = 0; i < ELEM_SIZE; i++)
 	{
 		x_temp = x;
@@ -112,11 +124,11 @@ vector3 cordic_vector(int x, int y, double z)
 		{
 			x = x + (y >> i);
 			y = y - (x_temp >> i);
-			z = z + elem_angle[i]; 
+			z = z + float_to_fixed(elem_angle[i]); 
 		} else {
 			x = x - (y >> i);
 			y = y + (x_temp >> i);
-			z = z - elem_angle[i];
+			z = z - float_to_fixed(elem_angle[i]);
 		}
 	}
 	vector3 result;
@@ -140,31 +152,40 @@ vector3 cordic(int x, int y, double z, cordic_mode_t mode)
 	return result;
 }
 
-int cos_cordic(double theta)
+double cos_cordic(int theta)
 {
-	return cordic(1,0,theta, ROTATION).x;
+	return fixed_to_float(cordic(1,0,theta, ROTATION).x);
 }
 
-int sin_cordic(double theta)
+double sin_cordic(int theta)
 {
-	return cordic(1,0,theta, ROTATION).y;
+	return fixed_to_float(cordic(1,0,theta, ROTATION).y);
 }
 
 double arctan_div_cordic(int x, int y)
 {
-	return cordic(x,y,0, VECTOR).z;
+	return fixed_to_float(cordic(x,y,0, VECTOR).z);
 }
 
 double arctan_cordic(int x)
 {
-	return cordic(1,x,0, VECTOR).z;
+	return fixed_to_float(cordic(1,x,0, VECTOR).z);
+}
+
+void testing()
+{
+	int i;
+	for (i = 0; i < ELEM_SIZE; i++)
+	{
+		printf("i: %d rep %d",i, float_to_fixed(elem_angle[i]));
+	}
 }
 
 int main()
 {
-	printf("cos(45) int %d\n", cos_cordic(45) & SHIFT_MASK);
-	printf("Cos(45): %f\n", ((double)(cos_cordic(45) & SHIFT_MASK) / (1 << SHIFT)));
-	printf("Sin(90): %f\n", ((double)(sin_cordic(90) & SHIFT_MASK) / (1 << SHIFT)));
+	//testing();
+	printf("Cos(45): %f\n", cos_cordic(45));
+	printf("Sin(90): %f\n", sin_cordic(90));
 	printf("Arctan(5/4): %f\n", arctan_div_cordic(4,5));
 	printf("Arctan(2): %f\n", arctan_cordic(2));
 	return 0;
